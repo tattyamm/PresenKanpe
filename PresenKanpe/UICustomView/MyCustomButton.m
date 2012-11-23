2eb81ac4a6d0410a5be1645aa27d91be2d0c12c8
tattyamm <entatsu.appli+github@gmail.com>
2012-11-24 01:15:18 +0900

    custom viewをこれから作る

---
+++ PresenKanpe/UICustomView/MyCustomButton.m
@@ -0,0 +1,31 @@
+//
+//  MyCustomButton.m
+//  PresenKanpe
+//
+//  Created by entatsu on 2012/11/24.
+//  Copyright (c) 2012年 entatsu. All rights reserved.
+//
+
+#import "MyCustomButton.h"
+
+@implementation MyCustomButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
