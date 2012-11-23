377a4950f8b1d062400ab9e9dea626a89aeb0bd7
tattyamm <entatsu.appli+github@gmail.com>
2012-11-23 20:45:47 +0900

    UILabelの表示を調整。改行させた。

--- PresenKanpe/AboutViewController.m
+++ PresenKanpe/AboutViewController.m
@@ -21,43 +21,34 @@
     CGRect cgRectSize = [[UIScreen mainScreen] bounds]; //cgRectSize.size.widthで取得
     
     
-    //ラベル高さ調整の実験
-    NSString *labelStr = @"使い方\n1.プレゼン中に見るメモを入力\n2.プレゼン画面へ\n3.プレゼン中にタイマーとメモをみてしゃべる";
-    CGSize labelStrSize = [labelStr
-                   sizeWithFont:[UIFont boldSystemFontOfSize:13]
-                   constrainedToSize:CGSizeMake(cgRectSize.size.width,cgRectSize.size.height/2)
-                   lineBreakMode:UILineBreakModeWordWrap];
-    NSLog(@"labelStr height: %f", labelStrSize.height);
-    NSLog(@"cgRectSize height: %f", cgRectSize.size.height);
-    
-    
     //ラベル
+    NSString *labelStr = @"使い方\n1.プレゼン中に見るメモを入力\n2.プレゼン画面へ\n3.プレゼン中にタイマーとメモをみてしゃべる";
     UILabel* label = [[[UILabel alloc] initWithFrame:self.view.bounds] autorelease];
     label.text = labelStr;
+    label.lineBreakMode = UILineBreakModeWordWrap;
     label.numberOfLines = 0;
-    label.textAlignment = NSTextAlignmentCenter;
+    label.frame = CGRectMake(0,0,cgRectSize.size.width,cgRectSize.size.height/2);
     label.backgroundColor = [UIColor blackColor];
     label.textColor = [UIColor whiteColor];
-    label.frame = CGRectMake(0,0,cgRectSize.size.width,cgRectSize.size.height);
     [label sizeToFit];
-    label.textAlignment = UITextAlignmentLeft;
-    label.adjustsFontSizeToFitWidth = YES;
-    label.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [self.view addSubview:label];
     
-    //ボタン
-    UIButton* button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [button setTitle:@"画面遷移" forState:UIControlStateNormal];
-    [button sizeToFit];
-    CGPoint newPoint = self.view.center;
-    newPoint.y += 50;
-    button.center = newPoint;
+    //ボタン 作者twitterへのボタン
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:@"作者twitter" forState:UIControlStateNormal];
+    button.frame = CGRectMake(0,0,cgRectSize.size.width,40);
     button.autoresizingMask =
     UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    [[button layer] setCornerRadius:20.0f];
+    [[button layer] setMasksToBounds:YES];
+    [[button layer] setBorderWidth:2.0f];
+    [[button layer] setBackgroundColor:[[UIColor blackColor] CGColor]];
+    [[button layer] setBorderColor:[[UIColor whiteColor] CGColor]];
     [button addTarget:self
                action:@selector(buttonDidPush)
      forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
+
 }
 
 - (void)buttonDidPush {
