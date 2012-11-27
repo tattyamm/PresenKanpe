4b66dd630abf0715c406587ce1d079577a628833
tattyamm <entatsu.appli+github@gmail.com>
2012-11-28 03:41:30 +0900

    キーボードの高さにUITextViewが追従するようにした。iOS5対応。

--- PresenKanpe/MainViewController.m
+++ PresenKanpe/MainViewController.m
@@ -66,45 +66,66 @@
     //テキストフィールド
     textView_ = [[UITextView alloc] init];
     textView_.frame = self.view.bounds;
-    textView_.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    textView_.delegate = self;
-    textView_.text = @"実は好奇心のゆえに、しかれども予は予が画師たるを利器として、ともかくも口実を設けつつ、予と兄弟もただならざる医学士高峰をしいて、某の日東京府下の一病院において、渠が刀を下すべき、貴船伯爵夫人の手術をば予をして見せしむることを余儀なくしたり。\nその日午前九時過ぐるころ家を出でて病院に腕車を飛ばしつ。直ちに外科室の方に赴くとき、むこうより戸を排してすらすらと出で来たれる華族の小間使とも見ゆる容目よき婦人二、三人と、廊下の半ばに行き違えり。見れば渠らの間には、被布着たる一個七、八歳の娘を擁しつ、見送るほどに見えずなれり。これのみならず玄関より外科室、外科室より二階なる病室に通うあいだの長き廊下には、フロックコート着たる紳士、制服着けたる武官、あるいは羽織袴の扮装の人物、その他、貴婦人令嬢等いずれもただならず気高きが、あなたに行き違い、こなたに落ち合い、あるいは歩し、あるいは停し、往復あたかも織るがごとし。";
+    //textView_.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    //textView_.delegate = self;
+    textView_.text = @"実は好奇心のゆえに、しかれども予は予が画師たるを利器として、ともかくも口実を設けつつ、予と兄弟もただならざる医学士高峰をしいて、某の日東京府下の一病院において、渠が刀を下すべき、貴船伯爵夫人の手術をば予をして見せしむることを余儀なくしたり。\nその日午前九時過ぐるころ家を出でて病院に腕車を飛ばしつ。直ちに外科室の方に赴くとき、むこうより戸を排してすらすらと出で来たれる華族の小間使とも見ゆる容目よき婦人二、三人と、廊下の半ばに行き違えり。見れば渠らの間には、被布着たる一個七、八歳の娘を擁しつ、見送るほどに見えずなれり。これのみならず玄関より外科室、外科室より二階なる病室に通うあいだの長き廊下には、フロックコート着たる紳士、制服着けたる武官、あるいは羽織袴の扮装の人物、その他、貴婦人令嬢等いずれもただならず気高きが、あなたに行き違い、こなたに落ち合い、あるいは歩し、あるいは停し、往復あたかも織るがごとし。予は今門前において見たる数台の馬車に思い合わせて、ひそかに心に頷けり。渠らのある者は沈痛に、ある者は憂慮わしげに、はたある者はあわただしげに、いずれも顔色穏やかならで、忙しげなる小刻みの靴の音、草履の響き、一種寂寞たる病院の高き天井と、広き建具と、長き廊下との間にて、異様の跫音を響かしつつ、うたた陰惨の趣をなせり。";
     [self.view addSubview:textView_];
-    
-    /* キーボードに追従しないので、一旦コメントアウト
-    //テキストフィールド TODO キーボードサイズを取得し、iPhoneとiPadとiOS5でちょうど良く表示する
-    UITextView* textField = [[[UITextView alloc] init] autorelease];
-    textField.frame = CGRectMake(0, 30, cgRectSize.size.width, 100);
-    textField.backgroundColor = [UIColor whiteColor];
-    textField.textColor = [UIColor blackColor];
-    textField.layer.cornerRadius = 5.0f;//角丸
-    textField.layer.borderColor = [[UIColor blackColor] CGColor];
-    textField.layer.borderWidth = 1;
-    [[textField layer] setBorderColor:[[UIColor whiteColor] CGColor]];
-    [[textField layer] setBorderWidth:1.0];
-    textField.text = @"（TODO 前回の入力内容を追加）";
-    [self.view addSubview:textField];
-    
-    //カンペ画面へ キーボードと同時に出る　http://www.toyship.org/archives/82
-    UIView* accessoryView =[[[UIView alloc] initWithFrame:CGRectMake(0,0,cgRectSize.size.width,40)] autorelease];
-    accessoryView.backgroundColor = [UIColor grayColor];
-    // ボタンを作成する。　//TODO iPad向けの調整
-    UIButton* nextButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    nextButton.frame = CGRectMake(cgRectSize.size.width/3*2,0,cgRectSize.size.width/3*1,40);
-    [[nextButton layer] setCornerRadius:20.0f];
-    [[nextButton layer] setMasksToBounds:YES];
-    [[nextButton layer] setBorderWidth:2.0f];
-    [[nextButton layer] setBackgroundColor:[[UIColor blackColor] CGColor]];
-    [[nextButton layer] setBorderColor:[[UIColor whiteColor] CGColor]];
-    [nextButton setTitle:@"決定" forState:UIControlStateNormal];
-    // ボタンを押したときによばれる動作を設定する。
-    [nextButton addTarget:self action:@selector(goButtonDidPushKanpe) forControlEvents:UIControlEventTouchUpInside];
-    // ボタンをViewに貼る
-    [accessoryView addSubview:nextButton];
-    textField.inputAccessoryView = accessoryView;
-     */
+    //親ビューの高さが変更された時に追従するようにする
+    textView_.autoresizingMask = UIViewAutoresizingFlexibleHeight;
     
 }
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    //キーボード表示・非表示の通知の開始
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    //キーボード表示・非表示の通知を終了
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+//キーボードが表示された場合
+- (void)keyboardWillShow:(NSNotification *)aNotification {
+    //キーボードのCGRectを取得
+    CGRect keyboardRect = [[[aNotification userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    keyboardRect = [[self.view superview] convertRect:keyboardRect fromView:nil];
+    
+    //キーボードのanimationDurationを取得
+    NSTimeInterval animationDuration = [[[aNotification userInfo] objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    //メインビューの高さをキーボードの高さ分マイナスしたframe    //TODO ここをself.view.frameを使わずに、画面サイズから計算するようにすr。　http://smartgadgetlaboratory.blogspot.jp/2012/08/ioswidthheight.html
+    CGRect frame = self.view.frame;
+    frame.size.height = [[UIScreen mainScreen] applicationFrame].size.height - self.navigationController.navigationBar.frame.size.height - keyboardRect.size.height;    //ステータスバーを除く画面高さ - navigationBarの高さ - キーボードの高さ
+    //TODO ここから、ラベルの分の高さを引く
+    
+    //キーボードアニメーションと同じ間隔でメインビューの高さをアニメーションしつつ変更する。
+    //これでUITextViewも追従して高さが変わる。
+    [UIView beginAnimations:@"ResizeForKeyboard" context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    self.view.frame = frame;
+    [UIView commitAnimations];
+}
+//キーボードが非表示にされた場合（keyboardWillShowと同じことを高さを+してやっているだけ）
+- (void)keyboardWillHide:(NSNotification *)aNotification {
+    CGRect keyboardRect = [[[aNotification userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    keyboardRect = [[self.view superview] convertRect:keyboardRect fromView:nil];
+    
+    NSTimeInterval animationDuration = [[[aNotification userInfo] objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    CGRect frame = self.view.frame;
+    frame.size.height += keyboardRect.size.height;
+    
+    [UIView beginAnimations:@"ResizeForKeyboard" context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    self.view.frame = frame;
+    [UIView commitAnimations];
+}
+
 
 
 //画面表示時
@@ -121,6 +142,7 @@
 
 //編集開始
 - (void) textViewDidBeginEditing:(UITextView*)textView {
+    /*
     static const CGFloat kKeyboardHeight = 216.0;   //TODO iOS5以降では、変換候補が出るとサイズが変わる。
     
     //アニメーションの設定
@@ -132,6 +154,7 @@
     textViewFrame.size.height = self.view.bounds.size.height - kKeyboardHeight;
     textView.frame = textViewFrame;
     //（他にも移動したいviewがあるならここで操作する）
+     */
 }
 
 //編集終了
